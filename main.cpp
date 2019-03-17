a260d657f5b1d69392a5c20bae5023fcf2bb0bda
Joel Larsson <44014925+Rexxyy@users.noreply.github.com>
2019-03-17 04:06:56 +0100

    aded fil :)
    dis is program i maek!! am veri prud!!

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*******************************************************************
+
+Helo guis dis was rily hard for mi to maek but i did maek it misel!!
+
+*******************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float x, y;
+	float answer;
+
+	cin >> x;
+	cin >> y;
+	answer = x + y;
+	cout << answer << endl;
+
+	system("PAUSE");
+	return 0;
+}
